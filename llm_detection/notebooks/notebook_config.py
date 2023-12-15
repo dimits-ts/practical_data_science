cf1ac75f51610b5253c88b1ac492b2ffdc6a3e6b
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2023-12-15 18:53:55 +0200

    Rework augmentation export

--- llm_detection/notebooks/notebook_config.py
+++ llm_detection/notebooks/notebook_config.py
@@ -1,4 +1,13 @@
-INTERMEDIATE_DIR = "intermediate"
+import os
+
+
+# directories
+DATA_DIR = os.path.join("..", "data")
+OUTPUT_DIR = os.path.join("..", "output")
+INTERMEDIATE_DIR = os.path.join("..", "intermediate")
+
+
+# LOOV file names
 LOOV_RES_NAME = "loov_res.csv"
 LOOV_INPUT_NAME = "loov_input_data.csv"
 MODEL_FILE_NAME = "best_model.skops"
